d50f6077b59f990f18be64359b37155ca430c67a
dlucioyauh <dlucio.douglas@gmail.com>
2024-11-27 21:56:02 -0300

    adicionando chaves publicas e secreta  do stripe

--- pages/api/hello.ts
+++
@@ -1,8 +0,0 @@
-// pages/api/hello.ts
-import { NextApiRequest, NextApiResponse } from 'next';
-
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ message: 'Olá, mundo!' });
-};
-
-export default handler;
