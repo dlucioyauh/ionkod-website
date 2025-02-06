0e3bc8cf54527021519ef2730cfb068aae382706
dlucioyauh <dlucio.douglas@gmail.com>
2025-02-06 11:43:39 -0300

    Criando menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,30 @@
-// src/app/page.tsx
-import Link from 'next/link';
-import Head from 'next/head';
+"use client";
+import Link from "next/link";
 
 const HomePage = () => {
   return (
-    <>
-      <Head>
-        <title>Bem-vindo ao IonKod</title>
-        <link rel="preload" href="/path/to/file.js" as="script" />
-      </Head>
-      <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-gray-800 to-gray-500 text-white">
-        <h1 className="text-4xl font-bold mb-4">Bem-vindo ao IonKod!</h1>
-        <p className="text-xl mb-6">Adquira conhecimento prático e transforme sua carreira.</p>
-        <Link href="/payment">
-          <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-lg">
-            Ir para Pagamento
-          </button>
-        </Link>
-      </div>
-    </>
+    <div className="min-h-screen flex flex-col">
+      <header className="menu">
+        <nav>
+          <ul>
+            <li><Link href="#home">Início</Link></li>
+            <li><Link href="#sobre">Sobre</Link></li>
+            <li><Link href="#cursos">Cursos</Link></li>
+            <li><Link href="#contato">Contato</Link></li>
+          </ul>
+        </nav>
+      </header>
+
+      <section className="hero">
+        <h1>Bem-vindo ao <span>IonKod!</span></h1>
+        <p>Adquira conhecimento prático e transforme sua carreira.</p>
+        <Link href="/payment" className="btn">Adquirir Curso</Link>
+      </section>
+
+      <footer>
+        <p>&copy; 2024 IonKod. Todos os direitos reservados.</p>
+      </footer>
+    </div>
   );
 };
 
